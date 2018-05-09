50ca290de5e84def3c3e06eae08f8cc93564d291
白迎春 <953152868@qq.com>
2018-05-09 17:20:59 +0800

    Initial Commit

---
+++ PushDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PushDemo
+//
+//  Created by hxb on 2018/5/9.
+//  Copyright © 2018年 yc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
